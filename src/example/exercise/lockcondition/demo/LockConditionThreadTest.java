b783fa70314fdcf7cfe801de32e8e4cfe756fc76
blake <blake@ganguo.hk>
2018-12-01 19:34:57 +0800

    feature: Condition 的API使用

--- src/example/exercise/lockcondition/demo/LockConditionThreadTest.java
+++ src/example/exercise/lockcondition/demo/LockConditionThreadTest.java
@@ -1,10 +1,24 @@
 package example.exercise.lockcondition.demo;
 
 /**
- * @Description TODO
+ * @Description Condition 控制线程通信
+ * <p>
+ * ==> 0）Lock lock = new ReentrantLock();
+ * Condition condition = lock.newCondition();
+ * <p>
+ * ==> 1）condition.await();
+ * ==> 2）condition.signal();
+ * ==> 3）condition.signalAll();
+ * <p>
+ * ===========
  * @Author blake
  * @Date 2018/12/1 下午12:19
  * @Version 1.0
  */
 public class LockConditionThreadTest {
+
+    public static void main(String[] args) {
+
+    }
+
 }
